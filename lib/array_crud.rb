7abc54997bc96b304d3e83b22f3450f75741ac2e
dzeit216@gmail.com Zeit <dzeit216@gmail.com>
2018-03-22 14:57:35 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,37 @@
+require "pry"
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["Dolphins", "Yankees", "Knicks", "Rangers"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop(4)
+  binding.pry 
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.pop(1)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array[3]
 end
